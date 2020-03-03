4ab8f4f12cb2b2a83e2c9c127a7e951159c1337c
Erik Vullings <erik.vullings@gmail.com>
2020-03-03 19:37:37 +0100

    FIX Issue with missing branch numbers.

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,2 @@
-export const padLeft = (str: string | number, ch = ' ', len = 2): string =>
-  str.toString().length >= len ? str.toString() : padLeft(ch + str.toString(), ch, len);
+export const padLeft = (str?: string | number, ch = ' ', len = 2): string =>
+  str ? str.toString().length >= len ? str.toString() : padLeft(ch + str.toString(), ch, len) : undefined;
